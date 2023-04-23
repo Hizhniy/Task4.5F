370d094f2967762187e2a520b21000ce58d33390
Hizhniy <hizhniy@gmail.com>
2023-04-23 19:00:07 +0300

    Добавьте файлы проекта.

---
+++ Task4.5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        (string Name, string LastName, string Login, int LoginLength, bool HasPet, double Age, string[] favcolors) User;
+
+        for (int k = 0; k < 3; k++)
+        {
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию");
+            User.LastName = Console.ReadLine();
+
+            Console.WriteLine("Введите логин");
+            User.Login = Console.ReadLine();
+            User.LoginLength = User.Login.Length;
+
+            Console.WriteLine("Есть ли у вас животные? Да или Нет");
+            if (Console.ReadLine() == "Да")
+            {
+                User.HasPet = true;
+            }
+
+            Console.WriteLine("Введите возраст пользователя");
+            User.Age = Convert.ToInt32(Console.ReadLine());
+
+            User.favcolors = new string[3];
+            Console.WriteLine("Введите три любимых цвета пользователя");
+            for (int i = 0; i < User.favcolors.Length; i++)
+            {
+                User.favcolors[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
